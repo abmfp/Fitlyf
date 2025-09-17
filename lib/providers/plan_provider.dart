be065984d55c2717c15ff787a5b6b7a5f211c8d9
abmfp <abmfp2@gmail.com>
2025-09-17 12:42:49 +0530

    Update plan_provider.dart

--- lib/providers/plan_provider.dart
+++ lib/providers/plan_provider.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
-import '../models/weekly_plan.dart';
 
 class PlanProvider with ChangeNotifier {
-  final List<WeeklyPlan> _plan = [
-    WeeklyPlan(day: "Monday", workout: "Chest & Biceps"),
-    WeeklyPlan(day: "Tuesday", workout: "Back & Triceps"),
-    WeeklyPlan(day: "Wednesday", workout: "Legs & Shoulders"),
-    WeeklyPlan(day: "Thursday", workout: "Rest"),
-    WeeklyPlan(day: "Friday", workout: "Chest & Back"),
-  ];
+  Map<String, String> _weeklyPlan = {
+    "Monday": "Chest",
+    "Tuesday": "Back",
+    "Wednesday": "Legs",
+    "Thursday": "Shoulders",
+    "Friday": "Arms",
+    "Saturday": "Cardio",
+    "Sunday": "Rest",
+  };
 
-  List<WeeklyPlan> get plan => _plan;
+  Map<String, String> get weeklyPlan => _weeklyPlan;
 
-  void updateWorkout(String day, String newWorkout) {
-    final index = _plan.indexWhere((p) => p.day == day);
-    if (index != -1) {
-      _plan[index].workout = newWorkout;
-      notifyListeners();
-    }
+  void updateDayPlan(String day, String workout) {
+    _weeklyPlan[day] = workout;
+    notifyListeners();
   }
 }
